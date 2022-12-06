06e55feb1e178c50c58ad191656a7136b01e3cd0
KostyaKaraman <kostya.karaman44@gmail.com>
2022-12-06 23:02:04 +0200

    fixed

--- index.php
+++ index.php
@@ -4,28 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>HandMade CMS</title>
 </head>
 <body>
-    
-<?php
-
-    $res = 0;
-
-    $first_value = rand(-10,10);
-    $second_value = rand (0,10);
-
-    $res = $first_value + $second_value * (pow($second_value,2));
-
-    if($res >= 0) {
-        echo $first_value." and ".$second_value."=".$res;
-    }else{
-        echo "$res and $first_value";
-    }
+    <?php 
+       
 
 
-?>
-         
-
+    ?>
 </body>
 </html>

3e9cfcdf395212e084ec49c285969edeadecc762
KostyaKaraman <kostya.karaman44@gmail.com>
2022-12-21 23:29:29 +0200

    fixed

--- class_car.php
+++ class_car.php
@@ -1,3 +1,27 @@
 <?php
-    
+  class Car{
+    public $wheels = 4;
+    protected $hood = 1;
+    private $engine = 1;
+    var $doors = 4;
+
+
+    function ShowProperty($wheels){
+         echo $this -> hood;
+    }
+
+  }
+
+  $obj = new Car();
+
+//   echo $obj -> engine;
+  
+
+  class Semi extends Car {
+    function ShowProperty($wheels){
+        echo $this -> engine;
+   }
+  }
+  $semi = new Semi();
+
 ?>
